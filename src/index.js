5a52b5bbce3350fae599d646c77863e308d2db36
Andrey Marhalyk <aom@anadeainc.com>
2021-03-16 22:29:52 +0300

    Add bootstrap and react modal

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import './assets/scss/style.scss';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app.component';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(<App />, document.querySelector('#root'));
