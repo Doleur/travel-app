b7d3e7e887dd716c57d42941cbe780d13cea6880
Doleur <voleur16@gmail.com>
2021-03-17 20:37:55 +0300

    fix conflickt

--- src/components/sight-gallery/sight-gallery.component.jsx
+++ src/components/sight-gallery/sight-gallery.component.jsx
@@ -1,21 +1,25 @@
 import React from 'react';
 import ImageGallery from 'react-image-gallery';
 import 'react-image-gallery/styles/scss/image-gallery.scss';
-
-import './sight-gallery.scss'
+import PropTypes from 'prop-types';
+import './sight-gallery.scss';
 
 const SightGallery = ({ attractions, language }) => {
-
   const images = attractions.map((el) => {
     return {
       original: `${el.photo_url}`,
       thumbnail: `${el.photo_url}`,
       description: `${el.description[language]}`,
       thumbnailLabel: `${el.name[language]}`
-    }
+    };
   });
 
   return <ImageGallery items={images} />;
+};
+
+SightGallery.propTypes = {
+  attractions: PropTypes.array,
+  language: PropTypes.string
 };
 
 export default SightGallery;
